b82f3f3d2d250f3b7372f072f1fee41673a8e2ed
정해성 <gotjd0230@naver.com>
2024-11-09 05:26:42 +0900

    feat: 상품 돌려 놓는 기능

--- src/StoreController.js
+++ src/StoreController.js
@@ -1,4 +1,4 @@
-import { formatOrder, arrangeStocks, purchase } from './functions.js';
+import { formatOrder, arrangeStocks, purchaseCartItems } from './functions.js';
 import FileSystem from './io/FileSystem.js';
 import Promotion from './models/Promotion.js';
 import { InputView, OutputView } from './view/index.js';
@@ -15,10 +15,12 @@ class StoreController {
     const arrangedStocks = arrangeStocks(stocks);
     OutputView.printStocks(arrangedStocks);
 
+    const products = getProducts(arrangedStocks);
     const orderString = await InputView.getOrder();
     const orders = formatOrder(orderString);
-    const basket = purchase(orders, arrangedStocks);
-    OutputView.printReceipt(basket);
+    const cart = addItemsToCart(orders, products);
+    const receipt = purchaseCartItems(cart, products);
+    OutputView.printReceipt(receipt);
 
     // const answer = await Console.readLineAsync('멤버십 할인을 받으시겠습니까? (Y/N)\n');
     // isMembershiped();
--- src/functions.js
+++ src/functions.js
@@ -1,5 +1,6 @@
 import { ERROR_MESSAGES } from './cosntants/errorMessages.js';
-import Product from './models/product.js';
+import Product from './models/Product.js';
+import Promotion from './models/Promotion.js';
 import { sameNameWith } from './utils/common.js';
 
 export const arrangeStocks = (stocks) => {
@@ -7,37 +8,27 @@ export const arrangeStocks = (stocks) => {
   // 프로모션 속성에 프로모션 객체 적용
   const indexToInsert = [];
 
-  const products = stocks.map((p, index) => {
-    p.price = Number(p.price);
-    p.quantity = Number(p.quantity);
-    if (p.promotion === 'null') {
-      p.promotion = '';
-    }
-
-    if (p.promotion !== '' && stocks[index + 1].name !== p.name) {
+  const products = stocks.map(({ name, price, quantity, promotion }, index) => {
+    if (promotion !== 'null' && stocks[index + 1].name !== name) {
       indexToInsert.push(index);
     }
 
-    return p;
+    return {
+      name,
+      price: Number(price),
+      quantity: Number(quantity),
+      promotion: new Promotion(promotion),
+    };
   });
 
   indexToInsert.reverse().forEach((index) => {
     products.splice(index + 1, 0, {
       name: stocks[index].name,
-      price: stocks[index].price,
+      price: Number(stocks[index].price),
       quantity: 0,
-      promotion: '',
+      promotion: new Promotion('null'),
     });
   });
-
-  const productsExtendsPromotion = products.map((product) => {
-    return {
-      ...product,
-      promotion: new Promotion(product.promotion);
-    }
-  })
-
-  
 
   return products;
 };
@@ -52,26 +43,54 @@ export const formatOrder = (string) => {
   });
 };
 
-export const purchase = (orders, stocks) => {
-  const products = stocks.flatMap((stock) => {
+export const getProducts = (stocks) => {
+  return stocks.flatMap((stock) => {
     return Array.from({ length: stock.quantity }, () => {
       delete stock.quantity;
       return new Product(stock);
     });
   });
+};
 
-  const bascket = orders.map(({ name, quantity }) => {
+export const addToCart = (orders, products) => {
+  const cart = orders.map(({ name, quantity }) => {
     const bundle = products.filter(sameNameWith(name));
 
     if (bundle.length < quantity) throw new Error(ERROR_MESSAGES.overStock);
-    if (bundle.some((product) => product.promotion !== '')) {
-      products.splice(products.findIndex(sameNameWith(name)), quantity);
-    }
 
-    return { name, price, quantity, promotion };
+    if (!bundle[0].promotion.isValidPeriod()) bundle.reverse(); // 프로모션 기간이 아닌 경우 일반 재고를 먼저 사용합니다.
+
+    return products.splice(products.findIndex(sameNameWith(name)), quantity);
   });
 
-  return bascket;
+  return cart;
+};
+
+export const purchaseCartItems = (cart, products) => {
+  const receipt = cart;
+
+  cart.forEach((bundle) => {
+    const thisPromotion = bundle[0].promotion;
+    const buyGetSum = thisPromotion.getData().buy + thisPromotion.getData().get;
+
+    if (thisPromotion.isValidPeriod()) {
+      const 프로모션상품개수 = bundle.reduce(
+        (acc, { promotion }) => acc + Number(promotion.isValidPeriod()),
+        1,
+      );
+
+      const 미적용상품개수 = bundle.length - Math.floor(프로모션상품개수 / buyGetSum) * buyGetSum;
+      products.push(bundle.splice(bundle.length - 미적용상품개수, 미적용상품개수));
+      return;
+    }
+  });
+
+  // 상품 제외
+  // 상품 추가
+  // 증정 적용
+  // 영수증 생성
+
+  return receipt;
 };
 
 export const isMembershiped = async () => {
