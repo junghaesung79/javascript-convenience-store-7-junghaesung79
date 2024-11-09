4d93800c53b3723130d00c6e13090ebcd770b1d2
정해성 <gotjd0230@naver.com>
2024-11-10 03:18:41 +0900

    refactor: 멤버십 입력 수정

--- src/StoreController.js
+++ src/StoreController.js
@@ -1,5 +1,5 @@
 import { Console } from '@woowacourse/mission-utils';
-import { formatOrder, arrangeStocks, addToCart, getProducts } from './functions.js';
+import { formatOrder, arrangeStocks, addToCart, getProducts, isMembershiped } from './functions.js';
 import FileSystem from './io/FileSystem.js';
 import Promotion from './models/Promotion.js';
 import PurchaseService from './services/PurchaseService.js';
@@ -22,10 +22,8 @@ class StoreController {
     const orders = formatOrder(orderString);
     const cart = addToCart(orders, products);
     const receipt = PurchaseService.purchaseCartItems(cart, products);
-    OutputView.printReceipt(receipt);
-
-    // const answer = await Console.readLineAsync('멤버십 할인을 받으시겠습니까? (Y/N)\n');
-    // isMembershiped();
+    const answer = InputView.askMembership();
+    OutputView.printReceipt(receipt, isMembershiped(answer));
   }
 }
 
--- src/view/InputView.js
+++ src/view/InputView.js
@@ -2,7 +2,13 @@ import Reader from '../io/Reader.js';
 
 class InputView {
   static async getOrder() {
-    return await Reader.readLine('구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])\n');
+    return await Reader.readLine(
+      '구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])\n',
+    );
+  }
+
+  static async askMembership() {
+    return await Reader.readLine('멤버십 할인을 받으시겠습니까? (Y/N)\n');
   }
 }
 
